8f98b45367ef9efa7fcc1875a5802557a3573b62
Sydney B <sydglyph@gmail.com>
2021-09-28 16:25:48 -0400

    Created 3 buttons, one for rock paper and scissors. Created eventListener that records when each button is clicked...that's pretty much it.

--- rps.js
+++ rps.js
@@ -1,79 +1,89 @@
+const playerSelection = playerPlay();
+const computerSelection = computerPlay();
+let playerWins = 0;
+let playerLoses = 0;
+let gameTies = 0;
+
+function playerPlay(){
+    const rpsChoice = document.querySelectorAll("button");
+    rpsChoice.forEach ((button) => {
+        button.addEventListener("click", () =>{
+            playerAnswer = button.id;
+            console.log(playerAnswer);
+            return playerAnswer;
+        })
+    })
+}
+
+function computerPlay() {
     const rpsOptions = ["rock", "paper", "scissors"];
-    const computerSelection = computerPlay();
-    let playerWins = 0;
-    let playerLoses = 0;
-    let gameTies = 0;
+    let computerAnswer = rpsOptions[Math.floor(Math.random()*rpsOptions.length)];
+    return computerAnswer;
+}
 
-    function computerPlay() {
-        let computerAnswer = rpsOptions[Math.floor(Math.random()*rpsOptions.length)];
-        return computerAnswer;
-    }
+function round(playerSelection, computerSelection) {
 
-    function round(playerSelection, computerSelection) {
-        
-        switch (playerSelection) {
-            case "rock":
-                if (computerSelection == "rock") {
-                    gameTies++;
-                    return "It's a tie! You both chose " + playerSelection + ".";
-                }
-                else if (computerSelection == "paper") {
-                    playerLoses++;
-                    return "You lose, " + computerSelection + " beats " + playerSelection + "!";
-                }
-                else if (computerSelection == "scissors") {
-                    playerWins++;
-                    return "You win, " + playerSelection + " beats " + computerSelection + "!";
-                }
-                else {
-                    return "Hmm, something went wrong!";
-                }
-                break;
-            case "paper":
-                if (computerSelection == "paper") {
-                    gameTies++;
-                    return "It's a tie! You both chose " + playerSelection + ".";
-                }
-                else if (computerSelection == "scissors") {
-                    playerLoses++;
-                    return "You lose, " + computerSelection + " beats " + playerSelection + "!";
-                }
-                else if (computerSelection == "rock") {
-                    playerWins++;
-                    return "You win, " + playerSelection + " beats " + computerSelection + "!";
-                }
-                else {
-                    return "Hmm, something went wrong!";
-                }
-                break;
-            case "scissors":
-                if (computerSelection == "scissors") {
-                    gameTies++;
-                    return "It's a tie! You both chose " + playerSelection + ".";
-                }
-                else if (computerSelection == "rock") {
-                    playerLoses++;
-                    return "You lose, " + computerSelection + " beats " + playerSelection + "!";
-                }
-                else if (computerSelection == "paper") {
-                    playerWins++;
-                    return "You win, " + playerSelection + " beats " + computerSelection + "!";
-                }
-                else {
-                    return "Hmm, something went wrong!";
-                }
-                break;
-            default:
-                console.log("Incorrect input.");
-        }
+    switch (playerSelection) {
+        case "rock":
+            if (computerSelection == "rock") {
+                gameTies++;
+                return "It's a tie! You both chose " + playerSelection + ".";
+            }
+            else if (computerSelection == "paper") {
+                playerLoses++;
+                return "You lose, " + computerSelection + " beats " + playerSelection + "!";
+            }
+            else if (computerSelection == "scissors") {
+                playerWins++;
+                return "You win, " + playerSelection + " beats " + computerSelection + "!";
+            }
+            else {
+                return "Hmm, something went wrong!";
+            }
+            break;
+        case "paper":
+            if (computerSelection == "paper") {
+                gameTies++;
+                return "It's a tie! You both chose " + playerSelection + ".";
+            }
+            else if (computerSelection == "scissors") {
+                playerLoses++;
+                return "You lose, " + computerSelection + " beats " + playerSelection + "!";
+            }
+            else if (computerSelection == "rock") {
+                playerWins++;
+                return "You win, " + playerSelection + " beats " + computerSelection + "!";
+            }
+            else {
+                return "Hmm, something went wrong!";
+            }
+            break;
+        case "scissors":
+            if (computerSelection == "scissors") {
+                gameTies++;
+                return "It's a tie! You both chose " + playerSelection + ".";
+            }
+            else if (computerSelection == "rock") {
+                playerLoses++;
+                return "You lose, " + computerSelection + " beats " + playerSelection + "!";
+            }
+            else if (computerSelection == "paper") {
+                playerWins++;
+                return "You win, " + playerSelection + " beats " + computerSelection + "!";
+            }
+            else {
+                return "Hmm, something went wrong!";
+            }
+            break;
+        default:
+            console.log("Incorrect input.");
     }
+}
 
-    function game() {
-        for (i = 1; i <= 5; i++) {
-            console.log("Round " + i);
-            let playerAnswer = window.prompt("Choose rock, paper, or scissors.");
-            console.log("You chose " + playerAnswer + ", your opponent chose " + computerSelection)
-            console.log(round(playerAnswer.toLowerCase(), computerSelection));
-        }
-        console.log("You won " + playerWins + " times, lost " + playerLoses + " times, and got a tie " + gameTies + " times.")
-    }
+
+// function game() {
+//     //let playerAnswer = window.prompt("Choose rock, paper, or scissors.");
+//     console.log("You chose " + playerSelection + ", your opponent chose " + computerSelection)
+//     console.log(round(playerSelection, computerSelection));
+//     console.log("You won " + playerWins + " times, lost " + playerLoses + " times, and got a tie " + gameTies + " times.")
+// }
